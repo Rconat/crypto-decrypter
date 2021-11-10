fdabe226972f248280da9fceba8216237f78a337
ryzecous <r3j9c89@gmail.com>
2021-11-09 21:05:32 -0500

    generateRandomWord function and word array

--- crypto-decrypter/src/components/crypto-decrypter/crypto-decrypter.js
+++ crypto-decrypter/src/components/crypto-decrypter/crypto-decrypter.js
@@ -7,12 +7,16 @@ function CryptoDecrypter() {
     const [userScore, setUserScore] = useState(0)
     const [userEntry, setUserEntry] = useState()
     const [randomNums, setRandomNums] = useState([])
+    const [randomWord, setRandomWord] = useState([])
     const [answer, setAnswer] = useState()
 
     //Creating an array to map the alphabet using character codes
     const alpha = Array.from(Array(26)).map((e, i) => i + 65);
     const alphabet = alpha.map((x) => String.fromCharCode(x))
     console.log(alphabet);
+
+    //Pool of words to be chosen at random
+    const words = ["ethereum", "bitcoin", "blockchain", "coinbase", "altcoin", "exchange", "genesis", "mining", "satoshi", "nakomoto", "token", "wallet", "stablecoin", "public", "private", "market", "cryptocurrency", "decentralization", "account", "address", "airnode", "algorithm", "allocation", "anonymous", "asynchronous", "authentication", "bandwidth", "benchmark", "bubble", "capital", "capitulation", "chain", "chargeback", "cipher", "collateral", "confirmation", "contract", "correction", "cryptoasset", "cryptography", "cypherpunk"]
 
     const textInput = React.useRef()
     const clearInput = () => (textInput.current.value = "")
@@ -46,26 +50,50 @@ function CryptoDecrypter() {
 
         let localRandomNums = []
 
-        while(localRandomNums.length < 27) {
+        while(localRandomNums.length < 26) {
             var r = Math.floor(Math.random() * 100) + 1
             if (localRandomNums.indexOf(r) === -1 )
             localRandomNums.push(r)
         }
 
         setRandomNums(localRandomNums)
+        console.log(localRandomNums)
     }
 
     //Grab a word from the list of possible words to use in the decryption code
     const generateRandomWord = () => {
 
-        let localRandomWords = []
+        let localRandomWord = []
+
+        var r = Math.floor(Math.random() * words.length)
+
+        localRandomWord.push(words[r])
+
+        // console.log(localRandomWord)
+        setRandomWord(localRandomWord)
     }
+
+
 
     const checkScore = () => {
         if (userScore === 10) {
             console.log("You have passed the Crypto Dectyptor game!")
         }
     }
+
+    //Generate random numbers on page load
+    useEffect(() => {
+        generateRandomNums()
+        generateRandomWord()
+    }, [])
+
+    // useEffect(() => {
+    //     //Checking user input against the random word
+    //     checkCode()
+
+    //     //Clearing the user input
+    //     clearInput()
+    // })
 
     return <div id="cryptoDecrypterGame" className="container">
         <div className="row">
